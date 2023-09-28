fdf7fff284dc7e20c626407107e3f4b120f58392
JibrilTurner <121262046+JibrilTurner@users.noreply.github.com>
2023-09-28 10:27:48 -0500

    Add files via upload

---
+++ 1.2'.py
@@ -0,0 +1,17 @@
+
+#{name} = input({value})
+#print("value" + uname)
+
+def quiz1_2():
+username = input("Enter username:")
+print("Username is: " + username)
+    return 3.3
+
+
+
+def test1_2(string):
+    # Add code to test if 'string' is a string here
+    if isinstance(string, str):
+        return True
+    else:
+        return False
